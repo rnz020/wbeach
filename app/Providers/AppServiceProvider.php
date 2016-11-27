63426a4ae62600af669a1f0785a283f8b28d36c3
Ramses <ramses.cr02@gmail.com>
2016-11-27 02:58:30 -0500

    quitar public_htmL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-         $this->app->bind('path.public', function() {
-          return base_path().'/../public_html';
-        });
     }
 }
