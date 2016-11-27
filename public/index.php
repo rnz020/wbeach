c83028765f24e6bfa3708149ad61493d611422e7
Ramses <ramses.cr02@gmail.com>
2016-11-27 02:41:00 -0500

    inicio

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../wbeach/bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../wbeach/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../wbeach/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
